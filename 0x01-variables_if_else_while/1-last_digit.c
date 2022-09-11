0f8415f28b314aa21323db1da14e3b951f57d871
Kariuki33 <danqarees@gmail.com>
2022-09-11 06:10:48 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 *main - print if the number is positive, zero, or negative
 *
 *Description: using the main function
-*this program prints "Programming is positive, zero, or negative
+*this program print "Programming is positive, zero, or negative
 *Return: 0
 */
 int main(void)
@@ -23,7 +23,7 @@ if (l > 5)
 }
 else if (l == 0)
 {
-	printf("Last digits of %d is %d and is 0\n", n, l);
+	printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
 {
