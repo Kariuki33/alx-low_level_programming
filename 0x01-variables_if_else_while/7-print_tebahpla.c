4b7b26202e77f51192a23f6348d98a2435aeb2a7
Kariuki33 <danqarees@gmail.com>
2022-09-11 04:15:24 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
-*main - Entry point
+*main - print if the number is positive, zero, or negative
 *
-*Return: Always 0 (Success)
+*Description: using the main function
+*this program prints "Programming is positive, zero, or negative
+*Return: 0
 */
-int main(void)
+int main (void)
 {
-char low;
-for (low = 'z'; low >= 'a'; low--)
-	putchar(low);
-	putchar('\n');
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
 return (0);
 }
