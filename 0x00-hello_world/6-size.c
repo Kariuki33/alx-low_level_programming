a4abd245e1b533378744ca07bc23959435d18745
Kariuki33 <danqarees@gmail.com>
2022-09-11 07:21:11 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
+*main - print the string in the put function
 *
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle  
-* Return: 0
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
 */
-int main(void) 
+int main(void)
 {
 	char c;
-	int i; 
+	int i;
 	long li;
-	long long lli; 
+	long long lli;
 	float f;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
