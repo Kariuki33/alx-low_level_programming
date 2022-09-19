94706f195691331b4f099f0ba22210e76ad728cc
Kariuki33 <danqarees@gmail.com>
2022-09-19 02:06:13 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,12 @@
 /**
  * swap_int - function that swaps the values of two intergers.
  * @a: this is the first entry
- * @b: this si the second entry
+ * @b: this is the second entry
  *
  *
  */
-void swap_int(int *a, int *b);
+
+void swap_int(int *a, int *b)
 {
 	int changed;
 
