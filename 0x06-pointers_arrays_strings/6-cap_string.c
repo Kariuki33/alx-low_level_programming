00c2285ea0d18ab77d5a1bad7d93c2a8b4c89938
Kariuki33 <danqarees@gmail.com>
2022-09-22 03:32:46 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
 *cap_string - function that capitalize first character of a word
 *@str: string to capitalize
 *Return: returns the capitalized string
 */
+
 char *cap_string(char *str)
 {
 	int index = 0;
@@ -17,6 +19,7 @@ char *cap_string(char *str)
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
 				str[index - 1] == ';' ||
 				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
