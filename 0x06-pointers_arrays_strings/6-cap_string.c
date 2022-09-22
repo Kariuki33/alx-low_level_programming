b5de8d8af6583471d44b495c9806ee58ceda34ca
Kariuki33 <danqarees@gmail.com>
2022-09-22 03:11:28 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,60 +1,32 @@
-#include "main.hh"
-
+#include "main.h"
 /**
-* cap_string - function that capitalize first character of a word
-* @str: The string to be capitalized.
-*
-* Return: A pointer to the changed string.
+*cap_string - function that capitalize first character of a word
+*@str: string to capitalize
+*Return: returns the capitalized string
 */
+char *cap_string(char *str)
+{
+	int index = 0;
 
-char *cap_string(char *str)                                                                                                 
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-{                                                                                                                           
-
-        int index = 0;                                                                                                      
-
-                                                                                                                            
-
-        while (str[++index])                                                                                                
-
-        {                                                                                                                   
-
-                while (!(str[index] >= 'a') && (str[index] <= 'z'))                                                         
-
-                        index++;                                                                                            
-
-                                                                                                                            
-
-                if (str[index - 1] == ' ' ||                                                                                
-
-                                str[index - 1] == '\t' ||                                                                   
-
-                                str[index - 1] == '\n' ||                                                                   
-
-                                str[index - 1] == ',' ||                                                                    
-
-                                str[index - 1] == ';' ||                                                                    
-
-                                str[index - 1] == '.' ||                                                                    
-
-                                str[index - 1] == '!' ||                                                                    
-
-                                str[index - 1] == '?' ||                                                                    
-
-                                str[index - 1] == '"' ||                                                                    
-
-                                str[index - 1] == '(' ||                                                                    
-
-                                str[index - 1] == ')' ||                                                                    
-
-                                str[index - 1] == '{' ||                                                                    
-
-                                str[index - 1] == '}' )                                                                     
-
-                        str[index] -= 32;                                                                                   
-
-        }                                                                                                                   
-
-        return (str);                                                                                                       
-
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
+	}
+	return (str);
 }
