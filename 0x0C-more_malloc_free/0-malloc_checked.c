afb3c69b3dbd2b4881fdf8476445ad7612884243
Kariuki33 <danqarees@gmail.com>
2022-10-06 06:24:14 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,13 +3,13 @@
  * malloc_checked -allocates memory using malloc.
  * @b: size of memory to be allocated.
  *
- * Return: pointer too allocated memory.
+ * Return: pointer to allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
 
-	if (p ==NULL)
+	if (p == NULL)
 		exit(98);
 
 	return (p);
